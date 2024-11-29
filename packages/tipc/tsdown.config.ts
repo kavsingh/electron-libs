6b776347ccf6aac65af7bda9a0f1ca8b26d6e4e8
Kav Singh <flakisms@gmail.com>
2024-11-29 19:05:31 +0100

    initial impl typed ipc

---
+++ packages/tipc/tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+	clean: true,
+	dts: true,
+	entry: [
+		"./src/common.ts",
+		"./src/main.ts",
+		"./src/preload.ts",
+		"./src/renderer.ts",
+	],
+	format: ["cjs", "esm"],
+});
