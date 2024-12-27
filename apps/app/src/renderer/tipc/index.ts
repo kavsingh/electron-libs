b5d49677a672932da1bcb97dc06e2856c4afd50c
Kav Singh <flakisms@gmail.com>
2024-12-27 22:15:47 +0100

    check serializers

--- apps/app/src/renderer/tipc/index.ts
+++
@@ -1,7 +0,0 @@
-import { createTIPCRenderer } from "tipc/renderer";
-
-import { serializer } from "#common/tipc/serializer-json";
-
-import type { AppTIPC } from "#common/tipc/definition";
-
-export const tipc = createTIPCRenderer<AppTIPC>({ serializer });
