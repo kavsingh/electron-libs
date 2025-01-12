d6a17221e23993f4a98c6090ec81922d5e02eb69
Kav Singh <flakisms@gmail.com>
2025-01-12 23:57:59 +0800

    reorgs and renames

--- apps/app/src/renderer/ipc/index.ts
+++
@@ -1,7 +0,0 @@
-import { createTypedIpcRenderer } from "electron-typed-ipc/renderer";
-
-import { serializer } from "#common/ipc/serializer";
-
-import type { AppIpcDefinition } from "#common/ipc/definition";
-
-export const ipc = createTypedIpcRenderer<AppIpcDefinition>({ serializer });
