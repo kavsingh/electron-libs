8e12a108a88b4d2378c48c8d6cd44662edbf43c7
Kav Singh <flakisms@gmail.com>
2025-04-03 00:56:06 +0200

    require both input and response to be specified

--- apps/example/main.ts
+++ apps/example/main.ts
@@ -14,8 +14,17 @@ void app.whenReady().then(() => {
 		webPreferences: { preload: path.resolve(dirname, "preload.cjs") },
 	});
 
-	tipc.ping.handleQuery(() => "pong");
+	const pongDisposer = tipc.ping.handleQuery(() => "pong");
 
 	if (!process.env["IS_E2E"]) appWindow.webContents.openDevTools();
 	void appWindow.loadFile(path.resolve(dirname, "app.html"));
+
+	const helloInterval = setInterval(() => {
+		tipc.helloNow.send(`hello now: ${Date.now()}`);
+	}, 500);
+
+	app.on("quit", () => {
+		clearInterval(helloInterval);
+		pongDisposer();
+	});
 });
