369e309ad1aa318c64558f51b6ef1d1157710c35
Kav Singh <flakisms@gmail.com>
2025-04-04 16:06:25 +0200

    finish up new main api

--- apps/example/main.ts
+++ apps/example/main.ts
@@ -4,27 +4,34 @@ import url from "node:url";
 import { createElectronTypedIpcMain } from "@kavsingh/electron-typed-ipc/main";
 import { app, BrowserWindow, ipcMain } from "electron";
 
-import type { AppIpcSchema } from "./common.ts";
+import { appIpcSchema } from "./common.ts";
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const tipc = createElectronTypedIpcMain<AppIpcSchema>(ipcMain);
+const tipc = createElectronTypedIpcMain(appIpcSchema, ipcMain);
 
 void app.whenReady().then(() => {
 	const appWindow = new BrowserWindow({
 		webPreferences: { preload: path.resolve(dirname, "preload.cjs") },
 	});
 
-	const pongDisposer = tipc.ping.handleQuery(() => "pong");
+	const disposeIpc = tipc.ipcHandleAndSend({
+		ping: () => "pong",
+
+		helloNow: (send) => {
+			const helloInterval = setInterval(() => {
+				send({ payload: `hello now: ${Date.now()}` });
+			}, 500);
+
+			return () => {
+				clearInterval(helloInterval);
+			};
+		},
+	});
 
 	if (!process.env["IS_E2E"]) appWindow.webContents.openDevTools();
 	void appWindow.loadFile(path.resolve(dirname, "app.html"));
 
-	const helloInterval = setInterval(() => {
-		tipc.helloNow.send(`hello now: ${Date.now()}`);
-	}, 500);
-
 	app.on("quit", () => {
-		clearInterval(helloInterval);
-		pongDisposer();
+		disposeIpc();
 	});
 });
