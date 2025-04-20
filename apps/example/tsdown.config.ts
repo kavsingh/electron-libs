b7fb208d840adba6d816fc7ad8da6256c9f8b14b
Kav Singh <flakisms@gmail.com>
2025-04-20 16:57:13 +0200

    refactor: drop separate schema

--- apps/example/tsdown.config.ts
+++ apps/example/tsdown.config.ts
@@ -3,14 +3,21 @@ import { defineConfig } from "tsdown";
 export default defineConfig([
 	{
 		clean: true,
-		entry: ["./main.ts", "./renderer.ts"],
-		target: ["node20", "chrome130"],
+		entry: ["./main.ts"],
+		target: ["node22"],
+		format: ["esm"],
+	},
+	{
+		entry: ["./renderer.ts"],
+		target: ["chrome134"],
+		noExternal: [/@kavsingh/],
 		format: ["esm"],
 		copy: [{ from: "./app.html", to: "./dist/app.html" }],
 	},
 	{
 		entry: ["./preload.ts"],
-		target: ["node20", "chrome130"],
+		target: ["node22", "chrome134"],
+		noExternal: [/@kavsingh/],
 		format: ["cjs"],
 	},
 ]);
