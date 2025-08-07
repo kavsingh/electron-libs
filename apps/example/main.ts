936c82fe474a137091948fd0f82b2ae847107767
Kav Singh <flakisms@gmail.com>
2025-08-07 20:28:57 +0200

    fix(example): avoid file:// protocol

--- apps/example/main.ts
+++ apps/example/main.ts
@@ -1,15 +1,46 @@
 import path from "node:path";
 import url from "node:url";
 
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, protocol, net } from "electron";
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
+protocol.registerSchemesAsPrivileged([{ scheme: "app" }]);
+
 void app.whenReady().then(() => {
+	protocol.handle("app", appProtocolHandler);
+
 	const appWindow = new BrowserWindow({
 		webPreferences: { preload: path.resolve(dirname, "preload.cjs") },
 	});
 
 	if (!process.env["IS_E2E"]) appWindow.webContents.openDevTools();
-	void appWindow.loadFile(path.resolve(dirname, "app.html"));
+	void appWindow.loadURL("app://dist/app.html");
 });
+
+async function appProtocolHandler(request: Request): Promise<Response> {
+	const { host, pathname } = new URL(request.url);
+
+	if (host !== "dist") {
+		return new Response("not found", {
+			status: 400,
+			headers: { "content-type": "text/html" },
+		});
+	}
+
+	const pathToServe = path.resolve(dirname, pathname.replace(/^\//, ""));
+	const relativePath = path.relative(dirname, pathToServe);
+	const isSafe =
+		relativePath &&
+		!relativePath.startsWith("..") &&
+		!path.isAbsolute(relativePath);
+
+	if (!isSafe) {
+		return new Response(`unsafe path: ${pathname}`, {
+			status: 400,
+			headers: { "content-type": "text/html" },
+		});
+	}
+
+	return net.fetch(url.pathToFileURL(pathToServe).href);
+}
--- apps/example/renderer.ts
+++ apps/example/renderer.ts
@@ -4,8 +4,11 @@ export function mount() {
 	if (!(appMount instanceof HTMLElement)) throw new Error("no valid #app");
 
 	appMount.innerHTML = `
+		<pre>${JSON.stringify(window.location, null, 2)}</pre>
 		<button data-click="ping">ping</button>
+		<button data-click="post">post</button>
 		<div data-display="pong"></div>
+		<div data-display="posts"></div>
 	`;
 
 	document.querySelector("[data-click=ping]")?.addEventListener("click", () => {
@@ -15,6 +18,18 @@ export function mount() {
 			display.innerHTML += `<br/>${window.api.ping()}`;
 		}
 	});
+
+	document.querySelector("[data-click=post]")?.addEventListener("click", () => {
+		window.postMessage("message", "*");
+	});
+
+	window.onmessage = (event) => {
+		const display = document.querySelector("[data-display=posts]");
+
+		if (display instanceof HTMLElement) {
+			display.innerHTML += `<br/>${event.data} (${event.origin})`;
+		}
+	};
 }
 
 document.addEventListener("DOMContentLoaded", mount);
