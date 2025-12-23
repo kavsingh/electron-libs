bc426bfdf1704c2afb6c68b5bfc256ffe10ab1e0
Kav Singh <flakisms@gmail.com>
2025-12-23 12:46:10 +0100

    build(deps): update deps

--- apps/example/tsdown.config.ts
+++ apps/example/tsdown.config.ts
@@ -12,10 +12,7 @@ export default defineConfig([
 		target: ["chrome130"],
 		noExternal: [/@kavsingh/],
 		format: ["esm"],
-		copy: [
-			{ from: "./app.html", to: "./dist/app.html" },
-			{ from: "./app.css", to: "./dist/app.css" },
-		],
+		copy: [{ from: "./app.{html,css}", to: "./dist" }],
 	},
 	{
 		entry: ["./preload.ts"],
