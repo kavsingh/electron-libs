d453cea3e1f37305e4acb7ce680ee3384aaf3750
Kav Singh <flakisms@gmail.com>
2026-01-17 17:48:37 +0100

    re-org example app files by electron / render

--- apps/example/tsdown.config.ts
+++ apps/example/tsdown.config.ts
@@ -3,24 +3,24 @@ import { defineConfig } from "tsdown";
 export default defineConfig([
 	{
 		clean: true,
-		entry: ["./main.ts"],
+		entry: ["./electron/main.ts"],
 		target: ["node24"],
 		format: ["esm"],
 	},
 	{
-		entry: ["./renderer.ts"],
+		entry: ["./electron/preload.ts"],
+		target: ["node24"],
+		noExternal: [/@kavsingh/],
+		format: ["cjs"],
+		// tsdown warns on cjs builds which are required for sandboxed preload
+		// scripts. this warning becomes an error on CI and blocks workflows
+		failOnWarn: false,
+	},
+	{
+		entry: ["./renderer/app.ts"],
 		target: ["chrome130"],
 		noExternal: [/@kavsingh/],
 		format: ["esm"],
-		copy: [{ from: "./app.{html,css}", to: "./dist" }],
-	},
-	{
-		entry: ["./preload.ts"],
-		target: ["node24", "chrome130"],
-		noExternal: [/@kavsingh/],
-		format: ["cjs"],
-		// tsdown warns on cjs builds which are required for preload scripts
-		// this warning becomes an error on CI which blocks E2E
-		failOnWarn: false,
+		copy: [{ from: "./renderer/app.{html,css}", to: "./dist" }],
 	},
 ]);
