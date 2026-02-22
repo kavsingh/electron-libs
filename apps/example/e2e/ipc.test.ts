c0f5709534241e156569935e8295c0e8fa507fde
Kav Singh <flakisms@gmail.com>
2026-02-22 22:29:27 +0100

    fix(example): revert app fixture wrap

--- apps/example/e2e/ipc.test.ts
+++ apps/example/e2e/ipc.test.ts
@@ -1,17 +1,31 @@
-import { test, expect } from "./fixtures.ts";
+import { test, expect } from "@playwright/test";
+
+import { setupApplication, teardownApplication } from "./lib/application.ts";
+
+import type { ElectronApplication } from "@playwright/test";
 
 test.describe("ipc tests", () => {
-	test("it should be able to complete req / res over ipc", async ({
-		mainWindow,
-	}) => {
-		await mainWindow.getByRole("button", { name: "req" }).click();
+	let app: ElectronApplication;
 
-		await expect(mainWindow.getByText("res")).toBeVisible();
+	test.beforeEach(async () => {
+		app = await setupApplication();
 	});
 
-	test("it should be able to complete pub / sub over ipc", async ({
-		mainWindow,
-	}) => {
-		await expect(mainWindow.getByText("pong (ping)")).toBeVisible();
+	test.afterEach(async () => {
+		await teardownApplication(app);
+	});
+
+	test("it should be able to complete req / res over ipc", async () => {
+		const page = await app.firstWindow();
+
+		await page.getByRole("button", { name: "req" }).click();
+
+		await expect(page.getByText("res")).toBeVisible();
+	});
+
+	test("it should be able to complete pub / sub over ipc", async () => {
+		const page = await app.firstWindow();
+
+		await expect(page.getByText("pong (ping)")).toBeVisible();
 	});
 });
