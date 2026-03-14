858bbf0627213f7ab4f21e1e717ca431f9863d8f
Kav Singh <flakisms@gmail.com>
2026-03-15 00:30:30 +0100

    chore(electron-typed-ipc): prefer esm

--- packages/electron-typed-ipc/tsdown.config.ts
+++ packages/electron-typed-ipc/tsdown.config.ts
@@ -1,23 +1,40 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig([
+	// common should run in any platform
 	{
 		clean: true,
 		dts: { build: true },
-		target: ["node22", "chrome130"],
-		entry: [
-			"./src/common.ts",
-			"./src/main.ts",
-			"./src/renderer.ts",
-			"./src/test-renderer.ts",
-		],
-		format: ["cjs", "esm"],
+		platform: "neutral",
+		target: ["es2022"],
+		entry: ["./src/common.ts"],
+		format: ["esm"],
 	},
+	// preload scripts have access to DOM and a subset of Node.js and
+	// Electron APIs
 	{
 		clean: false,
 		dts: { build: true },
-		target: ["node22", "chrome130"],
+		target: ["es2022"],
 		entry: ["./src/preload.ts"],
 		format: ["cjs"],
+	},
+	// main process Node.js only
+	{
+		clean: false,
+		dts: { build: true },
+		platform: "node",
+		target: ["node22"],
+		entry: ["./src/main.ts"],
+		format: ["esm"],
+	},
+	// renderer process browser only
+	{
+		clean: false,
+		dts: { build: true },
+		platform: "browser",
+		target: ["chrome130"],
+		entry: ["./src/renderer.ts", "./src/test-renderer.ts"],
+		format: ["esm"],
 	},
 ]);
